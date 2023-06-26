e7834266c5393c6305533f3907bff43dedc0e4ba
ana-olima <anaclaraoliveiralima@yahoo.com.br>
2023-06-26 15:10:35 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/projeto/demo/repositories/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.projeto.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.demo.entities.Order;
+import com.projeto.demo.entities.User;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+	
+	
+	
+	
+
+}
