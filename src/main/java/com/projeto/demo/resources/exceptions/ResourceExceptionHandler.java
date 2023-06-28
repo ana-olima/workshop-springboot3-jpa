e20301bb55d360ea005cb1c76583c3cf0c99a6e2
ana-olima <anaclaraoliveiralima@yahoo.com.br>
2023-06-28 17:29:55 -0300

    Exception handling - delete

--- src/main/java/com/projeto/demo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projeto/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.projeto.demo.resources.exceptions;
 
 import java.time.Instant;
 
+import org.hibernate.boot.model.relational.Database;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -23,5 +24,12 @@ public class ResourceExceptionHandler {
 		
 	
 	}
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e,  HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(),status.value(), error, e.getMessage(), request.getRequestURI());
+				return ResponseEntity.status(status).body(err);
+		
 }
- 
+}
